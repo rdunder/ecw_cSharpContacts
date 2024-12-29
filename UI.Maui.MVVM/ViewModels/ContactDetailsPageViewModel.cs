e535b5879ed784e1df365385c557b30004a44bf8
Roger Dunder <73080083+rdunder@users.noreply.github.com>
2024-12-29 22:07:20 +0100

    implemented delete contact in details page

--- UI.Maui.MVVM/ViewModels/ContactDetailsPageViewModel.cs
+++ UI.Maui.MVVM/ViewModels/ContactDetailsPageViewModel.cs
@@ -1,16 +1,29 @@
 ﻿
 
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Lib.Main.Interfaces;
 using Lib.Main.Models;
+using UI.Maui.MVVM.Pages;
 
 namespace UI.Maui.MVVM.ViewModels;
 
 
 [QueryProperty("SelectedContact", "SelectedContact")]
-public partial class ContactDetailsPageViewModel : ObservableObject
+public partial class ContactDetailsPageViewModel(IContactService contactService) : ObservableObject
 {
+    IContactService _contactService = contactService;
+
     [ObservableProperty]
     private ContactModel _selectedContact = new();
 
-    
+    [RelayCommand]
+    public void DeleteContact()
+    {
+        if (_selectedContact != null)
+        {
+            _contactService.RemoveContact(_selectedContact.Id);
+            Shell.Current.GoToAsync(nameof(MainPage));
+        }
+    }
 }
